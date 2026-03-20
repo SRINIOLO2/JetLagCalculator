dcfb7138c521c2c5eb1cb4f4128a0462d4665fe3
Sasha R <riniolosasha@gmail.com>
2026-03-19 18:59:51 -0700

    fix(deployment): dynamic basePath for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let basePath = '';
+
+if (isGithubActions && process.env.GITHUB_REPOSITORY) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  basePath = `/${repo}`;
+}
+
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
+  basePath: basePath,
 };
 
 export default nextConfig;
